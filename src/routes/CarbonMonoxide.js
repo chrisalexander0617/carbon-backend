1d045a27b4f125cf391c2ec327dcf74e5c834283
Chris <chrisalexander0617@gmail.com>
2023-05-01 20:25:16 -0400

    added router cors

--- src/routes/CarbonMonoxide.js
+++ src/routes/CarbonMonoxide.js
@@ -4,6 +4,12 @@ const axios = require('axios');
 const cors = require('cors');
 
 router.use(cors());
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow the specified HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type'); // Allow the specified headers
+  next();
+});
 
 router.get('/:country', async (req, res) => {
   const country = req.params.country;
--- src/routes/Country.js
+++ src/routes/Country.js
@@ -4,6 +4,13 @@ const axios = require('axios');
 const cors = require('cors');
 
 router.use(cors());
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow the specified HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type'); // Allow the specified headers
+  next();
+});
+
 
 router.get('/', async (req, res) => {
   const url = 'https://api.v2.emissions-api.org/api/v2/countries.json'
