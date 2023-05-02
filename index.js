f5577be131ddf53af66520adfed74d79be949d57
Chris <chrisalexander0617@gmail.com>
2023-05-01 20:21:45 -0400

    Added cors header

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.use(
     })
 );
 app.use(cors())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow the specified HTTP methods
+    res.header('Access-Control-Allow-Headers', 'Content-Type'); // Allow the specified headers
+    next();
+});
 
 app.get('/nitrogendioxide', async (req, res) => {
     const url = 'https://api.v2.emissions-api.org/api/v2/nitrogendioxide/statistics.json?country=US&begin=2019-02-01&end=2019-03-01'
