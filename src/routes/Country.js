19f426d7f434e57f055b7ffd001a3c722034ddc1
Chris <chrisalexander0617@gmail.com>
2023-05-04 10:24:32 -0400

    removed uneeded variabel

--- src/routes/Country.js
+++ src/routes/Country.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const cors = require('cors');
 
 const baseUrl = process.env.BASE_URL || 'https://api.v2.emissions-api.org/api/v2';
 
--- src/routes/Methane.js
+++ src/routes/Methane.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const cors = require('cors');
-
 const baseUrl = process.env.BASE_URL || 'https://api.v2.emissions-api.org/api/v2';
 
 router.get('/:country', async (req, res) => {
